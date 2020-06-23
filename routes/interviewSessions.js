e4dd89e3a911754c5632cce7e71588e5b4347679
anhld <wolfwithcode@gmail.com>
2020-06-23 22:25:08 +0700

    Add api getProcessingTicketId to get ticket created today and status is active

--- routes/interviewSessions.js
+++ routes/interviewSessions.js
@@ -1,3 +1,4 @@
+const startOfDay = require('date-fns/startOfDay');
 const router = require('express').Router();
 const InterviewSession = require('../models/interview-session.model');
 const { Ticket } = require('../models/ticket.model');
@@ -9,6 +10,22 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error: '+ err));
 });
 
+router.route('/getProcessingTicketId').get((req, res) => {
+    InterviewSession
+    .findOne({"timeIn": {"$gte": startOfDay(new Date())} , "active":true})
+    .then( interview => {
+        console.log("interview ", interview);
+        let ticketId = ""
+        if( interview ){            
+            ticketId = interview.ticket._id;
+        } else {
+            ticketId = '';
+        }
+        
+        return res.json(ticketId);
+    })
+    .catch(err => res.status(400).json('Error: '+ err));        
+})
 router.route('/add').post((req, res) => {
     // const {username, description, skypeId, timeIn} = req.body.ticket;
     // const ticket = new Ticket()
@@ -26,6 +43,7 @@ router.route('/add').post((req, res) => {
             .catch(err => res.status(400).json('Error: ' + err));
         });
 });
+
 
 
 module.exports = router;
